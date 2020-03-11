6184dd9d8f006e40e2c5b0394003df213e8a6a47
Jinyang <deng0083@e.ntu.edu.sg>
2020-03-11 11:29:38 +0800

    Created OkHttp class. OkHttp is used to bring in the dataset

---
+++ app/src/main/java/com/example/studyhotspot/OkHttp.java
@@ -0,0 +1,53 @@
+package com.example.studyhotspot;
+
+import okhttp3.*;
+import java.io.IOException;
+import org.json.JSONObject;
+
+public class OkHttp {
+    // one instance, reuse
+    private final OkHttpClient httpClient = new OkHttpClient();
+
+    public static void main(String[] args) throws Exception {
+
+        OkHttp obj = new OkHttp();
+
+        System.out.println("CKAN Package Show");
+
+        // id of the wireless hotspots on data.gov.sg is 6b3f1e1b-257d-4d49-8142-1b2271d20143
+        String dataURL = obj.getURL("6b3f1e1b-257d-4d49-8142-1b2271d20143");
+        String data = obj.accessData(dataURL);
+    }
+
+    private String getURL(String id) throws Exception {
+
+        Request request = new Request.Builder()
+                .url("https://data.gov.sg/api/action/package_show?id=" + id)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            // Get response body
+            String jsonData = response.body().string();
+
+            JSONObject Jobject = new JSONObject(jsonData);
+
+            String url = Jobject.getJSONObject("result").getJSONArray("resources").getJSONObject(1).get("url").toString();
+            // if want data in kml format: set getJSONObject(0)
+            // if want data in geojson format: set getJSONObject(1)
+
+            return(url);
+        }
+    }
+
+    private String accessData(String dataURL) throws Exception {
+        Request request = new Request.Builder()
+                .url(dataURL)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+            return(response.body().string());
+        }
+    }
+}
