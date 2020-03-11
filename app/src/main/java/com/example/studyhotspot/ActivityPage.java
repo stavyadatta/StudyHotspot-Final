f9c74c5bbcbf702b2d630f7a5a4b6aa27e921621
yiyoupteltd <yiyoupteltd@gmail.com>
2020-03-11 12:34:26 +0800

    Create ActivityPage.java

---
+++ app/src/main/java/com/example/studyhotspot/ActivityPage.java
@@ -0,0 +1,16 @@
+package com.example.studyhotspot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page);
+    }
+}
+
+//Testing
