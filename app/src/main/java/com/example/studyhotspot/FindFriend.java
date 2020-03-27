deede2b43696fc03bb5fa9ca52ca908c9620a9c7
chuanbinp <35805397+chuanbinp@users.noreply.github.com>
2020-03-27 21:50:58 +0800

    FindFriend able to see if friends are added,adding,none

--- app/src/main/java/com/example/studyhotspot/FindFriend.java
+++ app/src/main/java/com/example/studyhotspot/FindFriend.java
@@ -10,30 +10,70 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
+import javax.annotation.Nullable;
+
 public class FindFriend extends AppCompatActivity {
     ArrayList<String> namelist = new ArrayList<>();
     ArrayList<String> emaillist = new ArrayList<>();
+    ArrayList<String> addedFriendList = new ArrayList<String>();
+    ArrayList<String> addingFriendList = new ArrayList<String>();
+    ArrayList<Integer> relationshipFriendList = new ArrayList<Integer>();
+
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friend);
         FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseAuth fAuth = FirebaseAuth.getInstance();
+        userID = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                addedFriendList = (ArrayList<String>) documentSnapshot.get("addedfriends");
+                addingFriendList = (ArrayList<String>) documentSnapshot.get("addingfriends");
+                //String emailll = documentSnapshot.getString("email");
+                //Log.d("ADDED FRIEND LIST", "List: "+((ArrayList<String>) documentSnapshot.get("addedfriends")).get(0));
+                //Log.d("ADDED FRIEND LIST", "Size: "+ addedFriendList.size());
+                Log.d("ADDED FRIEND LIST", "List: "+ addingFriendList.get(0));
+            }
+        });
+
         firebaseFirestore.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
+                        String email = document.getString("email");
+                        if(addedFriendList.contains(email)){
+                            relationshipFriendList.add(2); //2 means added
+                        }
+                        else if(addingFriendList.contains(email)){
+                            relationshipFriendList.add(1); // 1 means adding
+                        }
+                        else{
+                            relationshipFriendList.add(0); // 0 means stranger
+                        }
                         namelist.add(document.getString("fName"));
-                        emaillist.add(document.getString("email"));
+                        emaillist.add(email);
                     }
                     Log.d("tagsuccess", namelist.toString());
+                    Log.d("relationship_list", "0: "+ relationshipFriendList.get(0));
+                    Log.d("relationship_list", "1: "+ relationshipFriendList.get(1));
                     initRecyclerView();
 
                 } else {
@@ -45,10 +85,11 @@ public class FindFriend extends AppCompatActivity {
 
     }
 
+
     private void initRecyclerView(){
         Log.d("Recycler Users", "initRecyclerView: init recyclerview.");
         RecyclerView recyclerView = findViewById(R.id.recyclerUsers);
-        RecyclerViewUserAdapter adapter = new RecyclerViewUserAdapter(namelist, emaillist, this);
+        RecyclerViewUserAdapter adapter = new RecyclerViewUserAdapter(namelist, emaillist, relationshipFriendList, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
--- app/src/main/java/com/example/studyhotspot/RecyclerViewUserAdapter.java
+++ app/src/main/java/com/example/studyhotspot/RecyclerViewUserAdapter.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,11 +21,13 @@ public class RecyclerViewUserAdapter extends RecyclerView.Adapter<RecyclerViewUs
 
     private ArrayList<String> mfNames = new ArrayList<>();
     private ArrayList<String> meMails = new ArrayList<>();
+    private  ArrayList<Integer> relationshipList = new ArrayList<>();
     private Context mContext;
 
-    public RecyclerViewUserAdapter(ArrayList<String> mfNames, ArrayList<String> meMails, Context mContext) {
+    public RecyclerViewUserAdapter(ArrayList<String> mfNames, ArrayList<String> meMails, ArrayList<Integer> rsList, Context mContext) {
         this.mfNames = mfNames;
         this.meMails = meMails;
+        this.relationshipList = rsList;
         this.mContext = mContext;
     }
 
@@ -46,6 +49,20 @@ public class RecyclerViewUserAdapter extends RecyclerView.Adapter<RecyclerViewUs
         holder.fName.setText(mfNames.get(position));
         holder.eMail.setText(meMails.get(position));
 
+        int status = relationshipList.get(position);
+        if(status==0){
+            holder.plus0.setImageResource(R.drawable.plus_0);
+            holder.plus0.setVisibility(View.VISIBLE);
+        }
+        else if(status==1){
+            holder.pending1.setImageResource(R.drawable.pending_1);
+            holder.pending1.setVisibility(View.VISIBLE);
+        }
+        else{
+            holder.check2.setImageResource(R.drawable.check_2);
+            holder.check2.setVisibility(View.VISIBLE);
+        }
+
         holder.userlayout.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
@@ -64,11 +81,17 @@ public class RecyclerViewUserAdapter extends RecyclerView.Adapter<RecyclerViewUs
         RelativeLayout userlayout;
         TextView fName;
         TextView eMail;
+        ImageView plus0;
+        ImageView pending1;
+        ImageView check2;
 
         public ViewUserHolder(@NonNull View itemView) {
             super(itemView);
             fName = itemView.findViewById(R.id.user_name);
             eMail = itemView.findViewById(R.id.user_email);
+            plus0 = itemView.findViewById(R.id.plus0);
+            pending1 = itemView.findViewById(R.id.pending1);
+            check2 = itemView.findViewById(R.id.check2);
             userlayout = itemView.findViewById(R.id.user_layout);
         }
     }
