74362f04f6f6ffe8f9fb31abeaaeaa0f1838839f
chavalitpramotedham <chavalit@pramotedham.com>
2020-04-02 16:55:36 +0800

    2/4 3.22am chava

--- app/src/main/java/com/example/studyhotspot/ActivityPageMain.java
+++ app/src/main/java/com/example/studyhotspot/ActivityPageMain.java
@@ -315,7 +315,7 @@ public class ActivityPageMain extends AppCompatActivity {
         Log.d(TAG, "initRecyclerView: init recyclerview.");
 
         RecyclerView recyclerView2 = findViewById(R.id.recyclerview2);
-        RecyclerViewAdapter2 adapter2 = new RecyclerViewAdapter2(id2, mNames2, mImageUrls2, fI1, mImageUrls22, mImageUrls23, this, this);
+        RecyclerViewAdapter2 adapter2 = new RecyclerViewAdapter2(mNames2, mImageUrls2, fI1, mImageUrls22, mImageUrls23, this, this);
         recyclerView2.setAdapter(adapter2);
         recyclerView2.setLayoutManager(new LinearLayoutManager(this));
     }
